b7396630ee24b6aad5ad9a8e6883b6544132668e
Anton Khorev <tony29@yandex.ru>
2017-10-09 13:54:41 +0300

    init

---
+++ class-options.js
@@ -0,0 +1,10 @@
+'use strict'
+
+class ClassOptions {
+	constructor(data,columns) {
+		this.data=data
+		this.columns=columns
+	}
+}
+
+module.exports=ClassOptions
---
+++ diagram-output.js
@@ -0,0 +1,9 @@
+'use strict'
+
+class DiagramOutput {
+	constructor(options) {
+		this.$output=$("<thead><tr><th>TODO</th></tr></thead>")
+	}
+}
+
+module.exports=DiagramOutput
