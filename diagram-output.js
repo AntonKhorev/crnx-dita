8c61cb8225afe7074d87c4a13badeb9d4bde6688
Anton Khorev <tony29@yandex.ru>
2017-10-09 14:00:54 +0300

    output class ids

--- diagram-output.js
+++ diagram-output.js
@@ -3,6 +3,9 @@
 class DiagramOutput {
 	constructor(options) {
 		this.$output=$("<thead><tr><th>TODO</th></tr></thead>")
+		this.$output=$("<thead>").append("<tr>").append(
+			options.columns.map(classId=>`<th>${classId}</th>`)
+		)
 	}
 }
 
