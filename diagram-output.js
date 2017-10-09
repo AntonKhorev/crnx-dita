a9e5069ae1152887e76c9423a06d9275e76c5037
Anton Khorev <tony29@yandex.ru>
2017-10-09 14:46:16 +0300

    delete column button

--- diagram-output.js
+++ diagram-output.js
@@ -2,12 +2,24 @@
 
 class DiagramOutput {
 	constructor(options) {
-		this.$output=$("<thead><tr><th>TODO</th></tr></thead>")
-		this.$output=$("<thead>").append(
-			$("<tr>").append(
-				options.columns.map(classId=>`<th>${options.data[classId].name}</th>`)
+		const $output=$("<thead>")
+		const update=()=>{
+			$output.empty().append(
+				$("<tr>").append(
+					options.columns.map((classId,i)=>$("<th>").append(
+						$("<button>delete</button>").click(function(){
+							options.columns=options.columns.filter((classId,j)=>i!=j)
+							update()
+						})
+					))
+				),
+				$("<tr>").append(
+					options.columns.map(classId=>`<th>${options.data[classId].name}</th>`)
+				)
 			)
-		)
+		}
+		update()
+		this.$output=$output
 	}
 }
 
