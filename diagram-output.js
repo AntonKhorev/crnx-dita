7159d021e05006f3bd4d19f9d09427b736554ff6
Anton Khorev <tony29@yandex.ru>
2017-10-09 14:56:55 +0300

    move column buttons

--- diagram-output.js
+++ diagram-output.js
@@ -7,8 +7,20 @@ class DiagramOutput {
 			$output.empty().append(
 				$("<tr>").append(
 					options.columns.map((classId,i)=>$("<th>").append(
+						$("<button>←</button>").click(function(){
+							if (i<=0) return
+							options.columns=options.columns.map((classId,j)=>(j!=i&&j!=i-1?classId:options.columns[i-(i==j)]))
+							update()
+						})
+						," ",
 						$("<button>delete</button>").click(function(){
 							options.columns=options.columns.filter((classId,j)=>i!=j)
+							update()
+						})
+						," ",
+						$("<button>→</button>").click(function(){
+							if (i>=options.columns.length-1) return
+							options.columns=options.columns.map((classId,j)=>(j!=i&&j!=i+1?classId:options.columns[i+(i==j)]))
 							update()
 						})
 					))
