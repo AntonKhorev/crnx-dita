92534eec401340b098c7c1ea58648d6d8ac2188f
Anton Khorev <tony29@yandex.ru>
2017-10-10 22:13:13 +0300

    diagramLayoutWalk

--- diagram-layout-walk.js
+++ diagram-layout-walk.js
@@ -1,12 +1,38 @@
 'use strict'
 
+const nd={b:'t',t:'b',l:'r',r:'l',n:'n'}
+const dx={b:0,t:0,l:-1,r:+1,n:0}
+const dy={b:+1,t:-1,l:0,r:0,n:0}
+
 // layout as returned by diagram-layout.js
 // int x,y
 // inDirection {'n','b','l','r'}
 // inCallback(x,y,inDirection) -> true to stop
 // outCallback(x,y,inDirection,outDirection) -> true to stop
 const diagramLayoutWalk=(layout,x,y,inDirection,inCallback=()=>0,outCallback=()=>0)=>{
-	if (inCallback(x,y,inDirection)) return
+	const visited=layout.map(row=>row.map(cell=>({})))
+	const rec=(x,y,id)=>{
+		if (layout[y][x][id]) return
+		layout[y][x][id]=true
+		if (inCallback(x,y,id)) return
+		for (let od in nd) {
+			if (
+				od!=id &&
+				layout[y][x][id+od] &&
+				!outCallback(x,y,id,od)
+			) {
+				const x1=x+dx[od]
+				const y1=y+dy[od]
+				if (
+					x1>=0 && x1<layout[y].length &&
+					y1>=0 && y1<layout.length
+				) {
+					rec(x1,y1,nd[od])
+				}
+			}
+		}
+	}
+	rec(x,y,inDirection)
 }
 
 module.exports=diagramLayoutWalk
--- test/diagram-layout-walk.js
+++ test/diagram-layout-walk.js
@@ -4,24 +4,113 @@ const chai=require('chai')
 const expect=chai.expect
 const diagramLayoutWalk=require('../diagram-layout-walk')
 
+const getWalkCalls=(layout,x,y,inDirection='n',inCallback=()=>0,outCallback=()=>0)=>{
+	const calls=[]
+	diagramLayoutWalk(
+		layout,x,y,inDirection,
+		(...args)=>{calls.push(['i',...args]); return inCallback(...args)},
+		(...args)=>{calls.push(['o',...args]); return outCallback(...args)}
+	)
+	return calls
+}
+
 describe("diagramLayoutWalk",()=>{
 	it("walks the only element",()=>{
-		const calls=[]
-		diagramLayoutWalk([
+		const calls=getWalkCalls([
 			[{node:'a'}]
-		],0,0,'n',(...args)=>{calls.push(['i',...args])},(...args)=>{calls.push(['o',...args])})
-		expect(calls).to.have.deep.members([['i',0,0,'n']])
+		],0,0)
+		expect(calls).to.have.deep.members([
+			['i',0,0,'n'],
+		])
 	})
-	/*
 	it("walks 2-element chain",()=>{
-		assertDiagramLayout(
-			[
-				[{node:'a',b:true},{}],
-				[{rt:true},{bl:true}],
-				[{},{node:'b',t:true}],
-			],
-			['a','b'],{a:{},b:{a:true}}
-		)
+		const calls=getWalkCalls([
+			[{node:'a',bn:true},{}],
+			[{rt:true},{bl:true}],
+			[{},{node:'b',nt:true}],
+		],1,2)
+		expect(calls).to.have.deep.members([
+			['i',1,2,'n'],
+			['o',1,2,'n','t'],
+			['i',1,1,'b'],
+			['o',1,1,'b','l'],
+			['i',0,1,'r'],
+			['o',0,1,'r','t'],
+			['i',0,0,'b'],
+			['o',0,0,'b','n'],
+			['i',0,0,'n'],
+		])
 	})
-	*/
+	it("walks 3-element fork",()=>{
+	const calls=getWalkCalls([
+			[{node:'a',bn:true},{},{node:'b',bn:true}],
+			[{rt:true},{bl:true,br:true},{lt:true}],
+			[{},{node:'c',nt:true},{}],
+		],1,2)
+		expect(calls).to.have.deep.members([
+			['i',1,2,'n'],
+			['o',1,2,'n','t'],
+			['i',1,1,'b'],
+			['o',1,1,'b','l'], // fork left path
+			['i',0,1,'r'],
+			['o',0,1,'r','t'],
+			['i',0,0,'b'],
+			['o',0,0,'b','n'],
+			['i',0,0,'n'],
+			['o',1,1,'b','r'], // fork right path
+			['i',2,1,'l'],
+			['o',2,1,'l','t'],
+			['i',2,0,'b'],
+			['o',2,0,'b','n'],
+			['i',2,0,'n'],
+		])
+	})
+	it("walks 3-element chain",()=>{
+		const calls=getWalkCalls([
+			[{node:'a',bn:true},{},{}],
+			[{rt:true},{bl:true},{}],
+			[{},{node:'b',nt:true,bn:true},{}],
+			[{},{rt:true},{bl:true}],
+			[{},{},{node:'c',nt:true}],
+		],2,4)
+		expect(calls).to.have.deep.members([
+			['i',2,4,'n'],
+			['o',2,4,'n','t'],
+			['i',2,3,'b'],
+			['o',2,3,'b','l'],
+			['i',1,3,'r'],
+			['o',1,3,'r','t'],
+			['i',1,2,'b'],
+			['o',1,2,'b','n'],
+			['i',1,2,'n'],
+			['o',1,2,'n','t'],
+			['i',1,1,'b'],
+			['o',1,1,'b','l'],
+			['i',0,1,'r'],
+			['o',0,1,'r','t'],
+			['i',0,0,'b'],
+			['o',0,0,'b','n'],
+			['i',0,0,'n'],
+		])
+	})
+	it("walks 3-element chain with break at middle node",()=>{
+		const calls=getWalkCalls([
+			[{node:'a',bn:true},{},{}],
+			[{rt:true},{bl:true},{}],
+			[{},{node:'b',nt:true,bn:true},{}],
+			[{},{rt:true},{bl:true}],
+			[{},{},{node:'c',nt:true}],
+		],2,4,'n',(x,y,d)=>(x==1&&y==2&&d=='n'))
+		expect(calls).to.have.deep.members([
+			['i',2,4,'n'],
+			['o',2,4,'n','t'],
+			['i',2,3,'b'],
+			['o',2,3,'b','l'],
+			['i',1,3,'r'],
+			['o',1,3,'r','t'],
+			['i',1,2,'b'],
+			['o',1,2,'b','n'],
+			['i',1,2,'n'],
+		])
+	})
 })
